27958cf43b7c8f6e023b3027f724a06cc1206ee4
Barathlingam K <rkbarath5717@gmail.com>
2026-07-12 21:15:42 +0530

    Add files via upload

---
+++ FindMinimum.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class FindMinimum {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n=scn.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=scn.nextInt();
+        }
+        int left=0;
+        int right=n-1;
+        while(left<right){
+            int mid=left+(right-left)/2;
+            if(arr[mid]>arr[right]){
+                left=mid+1;
+            }
+            else{
+                right=mid;
+            }
+        }
+        System.out.println(arr[left]);
+    }
+}
